529b5b248d1e9912bc0df12da35e3637ff469359
andy92pac <andy.mpondo@gmail.com>
2018-03-26 19:27:23 +0100

    update

--- script.R
+++ script.R
@@ -28,3 +28,110 @@ ward.coil = NbClust(data = coil.data, method = "ward.D")
 ward.coil.part = ward.coil$Best.partition
 
 single.coil = NbClust(data = coil.data, method = "single")
+single.coil.part = single.coil$Best.partition
+
+complete.coil = NbClust(data = coil.data, method = "complete")
+complete.coil.part = complete.coil$Best.partition
+
+
+#5
+
+
+
+
+
+##########
+###jaffe###
+##########
+
+jaffe.data <- jaffe$X
+jaffe.label <- jaffe$y
+
+# Q3 : ACP
+pca.jaffe <- PCA(jaffe.data)
+
+# Q4 : NbClust
+kmeans.jaffe = NbClust(data = jaffe.data, method = "kmeans")
+kmeans.jaffe.part = kmeans.jaffe$Best.partition
+
+average.jaffe =NbClust(data = jaffe.data, method = "average")
+average.jaffe.part =average.jaffe$Best.partition
+
+ward.jaffe = NbClust(data = jaffe.data, method = "ward.D")
+ward.jaffe.part = ward.jaffe$Best.partition
+
+single.jaffe = NbClust(data = jaffe.data, method = "single")
+single.jaffe.part = single.jaffe$Best.partition
+
+complete.jaffe = NbClust(data = jaffe.data, method = "complete")
+complete.jaffe.part = complete.jaffe$Best.partition
+
+
+#5
+
+
+
+
+##########
+###mnist###
+##########
+
+mnist.data <- mnist$X
+mnist.label <- mnist$y
+
+# Q3 : ACP
+pca.mnist <- PCA(mnist.data)
+
+# Q4 : NbClust
+kmeans.mnist = NbClust(data = mnist.data, method = "kmeans")
+kmeans.mnist.part = kmeans.mnist$Best.partition
+
+average.mnist =NbClust(data = mnist.data, method = "average")
+average.mnist.part =average.mnist$Best.partition
+
+ward.mnist = NbClust(data = mnist.data, method = "ward.D")
+ward.mnist.part = ward.mnist$Best.partition
+
+single.mnist = NbClust(data = mnist.data, method = "single")
+single.mnist.part = single.mnist$Best.partition
+
+complete.mnist = NbClust(data = mnist.data, method = "complete")
+complete.mnist.part = complete.mnist$Best.partition
+
+
+#5
+
+
+
+##########
+###mfeat###
+##########
+
+mfeat.data <- mfeat$X
+mfeat.label <- mfeat$y
+
+# Q3 : ACP
+pca.mfeat <- PCA(mfeat.data)
+
+# Q4 : NbClust
+kmeans.mfeat = NbClust(data = mfeat.data, method = "kmeans")
+kmeans.mfeat.part = kmeans.mfeat$Best.partition
+
+average.mfeat =NbClust(data = mfeat.data, method = "average")
+average.mfeat.part =average.mfeat$Best.partition
+
+ward.mfeat = NbClust(data = mfeat.data, method = "ward.D")
+ward.mfeat.part = ward.mfeat$Best.partition
+
+single.mfeat = NbClust(data = mfeat.data, method = "single")
+single.mfeat.part = single.mfeat$Best.partition
+
+complete.mfeat = NbClust(data = mfeat.data, method = "complete")
+complete.mfeat.part = complete.mfeat$Best.partition
+
+
+#5
+
+
+
+
